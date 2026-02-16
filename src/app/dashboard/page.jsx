7597a66f8b516f28c9ee34f1ee7e1f559c2df8e4
pmayur02 <mayurpansare2000@gmail.com>
2026-02-16 12:16:00 +0530

    verify realtime working on prod.

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -17,19 +17,23 @@ export default function Dashboard() {
     checkUser()
     fetchBookmarks()
 
-    const channel = supabase
-      .channel('realtime-bookmarks')
-      .on(
-        'postgres_changes',
-        { event: '*', schema: 'public', table: 'bookmarks' },
-        () => {
-          fetchBookmarks()
+  const channel = supabase
+    .channel('realtime-bookmarks')
+    .on(
+      'postgres_changes',
+      { event: '*', schema: 'public', table: 'bookmarks' },
+      (payload) => {
+        // update state directly from the payload
+        if (payload.eventType === 'INSERT') {
+          setBookmarks(prev => [payload.new, ...prev])
+        } else if (payload.eventType === 'DELETE') {
+          setBookmarks(prev =>
+            prev.filter(b => b.id !== payload.old.id)
+          )
         }
-      )
-      .subscribe()
-
-    console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
-  console.log("SUPABASE KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+      }
+    )
+    .subscribe()
 
     return () => {
       supabase.removeChannel(channel)
